6daec37a94e07a2cee387537523098e6966ebe7b
ThanhDien2004 <phamthanhdien04052004@gmail.com>
2024-06-20 23:17:20 +0700

    ThanhDien

---
+++ QuanLyBanHang_TapHoa/Controllers/LoaiSPController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyBanHang_TapHoa.Models;
+
+namespace QuanLyBanHang_TapHoa.Controllers
+{
+    public class LoaiSPController : Controller
+    {
+        QL_TapHoaEntities db = new QL_TapHoaEntities(); 
+        // GET: LoaiSP
+        public ActionResult Index()
+        {
+            var loaisp = db.LOAISANPHAMs.ToList();
+            return View(loaisp);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(LOAISANPHAM lsp)
+        {
+            try
+            {
+                db.LOAISANPHAMs.Add(lsp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("LOI TAO MOI CATEGORY");
+            }
+        }
+    }
+}
---
+++ QuanLyBanHang_TapHoa/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyBanHang_TapHoa.Models;
+
+namespace QuanLyBanHang_TapHoa.Controllers
+{
+    public class LoginController : Controller
+    {
+        private QL_TapHoaEntities db = new QL_TapHoaEntities();
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(TAIKHOAN ad)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(ad.TenDangNhap)) ModelState.AddModelError(string.Empty, "Name không được để trống");
+
+                if (string.IsNullOrEmpty(ad.MatKhau)) ModelState.AddModelError(string.Empty, "Password không được để trống");
+                if (ModelState.IsValid)
+                {
+                    // tim khach hang co ten dang nhap va password hop le trong csdl
+                    var admin = db.TAIKHOANs.FirstOrDefault(k => k.TenDangNhap == ad.TenDangNhap && k.MatKhau == ad.MatKhau);
+                    if (admin != null)
+                    {
+                        ViewBag.ThongBao = " chuc mung ban dang nhap thanh cong";
+                        //luu vao session
+                        Session["TaiKhoan"] = admin;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.ThongBao = " ten dang nhap hoac mat khau khong dung";
+                    }
+                }
+            }
+            return View("Login");
+        }
+
+    }
+}
